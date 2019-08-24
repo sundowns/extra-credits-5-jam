ea9cb9ff62c8cc1d429d0c5fea69d236bbf060ce
Thomas Smallridge <tom@smallridge.com.au>
2019-08-24 15:11:16 +0800

    remove unused system

--- src/systems/init.lua
+++ src/systems/init.lua
@@ -4,6 +4,5 @@ return {
   input = require(PATH .. ".input"),
   rowing = require(PATH .. ".rowing"),
   motion = require(PATH .. ".motion"),
-  camera = require(PATH .. ".camera"),
-  sprite_renderer = require(PATH .. ".sprite_renderer")
+  camera = require(PATH .. ".camera")
 }
