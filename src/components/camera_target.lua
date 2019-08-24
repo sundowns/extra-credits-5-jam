551fb29d345442f84afa824b04c86b4ea57bd3ea
Thomas Smallridge <tom@smallridge.com.au>
2019-08-24 14:15:55 +0800

    add camera smoother

--- src/components/camera_target.lua
+++ src/components/camera_target.lua
@@ -1,7 +1,7 @@
 local camera_target =
   Component(
   function(e)
-    e.id = _util.s.randomString(8) -- replace with uuid library?
+    e.id = _util.s.randomString(8)
     e.camera = Camera(0, 0)
   end
 )
