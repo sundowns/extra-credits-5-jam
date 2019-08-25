88fc0bd52c2ce29e18840a57363dded1816e452b
Sundowns <tom@smallridge.com.au>
2019-08-25 19:13:14 +0800

    luacheck

--- src/systems/tutorial.lua
+++ src/systems/tutorial.lua
@@ -5,7 +5,7 @@ function tutorial:init()
   love.graphics.setFont(self.font)
 end
 
-function tutorial:draw_ui()
+function tutorial.draw_ui(_)
   local y_offset = 20
   love.graphics.setColor(_constants.TEXT_COLOUR)
   local x, y = 8, love.graphics.getHeight() - 8 - y_offset
