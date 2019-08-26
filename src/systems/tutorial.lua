3ace1ee04aaefb21bdfc3b7d8ee1cb6ec2d2828b
Sundowns <tom@smallridge.com.au>
2019-08-26 22:19:25 +0800

    update tutorial box size

--- src/systems/tutorial.lua
+++ src/systems/tutorial.lua
@@ -7,7 +7,7 @@ function tutorial.draw_ui(_)
   love.graphics.setFont(_fonts["TUTORIAL"])
   local y_offset = -20
   love.graphics.setColor(0.2, 0.2, 0.2, 0.8)
-  love.graphics.rectangle("fill", 0, 0, 335, 160)
+  love.graphics.rectangle("fill", 0, 0, 335, 135)
   love.graphics.setColor(_constants.COLOURS.TUTORIAL_TEXT)
   local x, y = 6, 6
   love.graphics.print("[ a ] / [ d ] - move paddle right / left", x, y)
