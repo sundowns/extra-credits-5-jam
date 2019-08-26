daf5ac326a4245bd4e56d561e59d9fee3527128a
Sundowns <tom@smallridge.com.au>
2019-08-26 20:58:34 +0800

    mappy

--- src/dialogue.lua
+++ src/dialogue.lua
@@ -4,7 +4,7 @@ return {
       Talkies.say("Lost Soul", "How do I talk?", {image = soulImg, font = inFont})
       Talkies.say("Charon", "You just did pal", {image = boatImg, font = inFont})
       Talkies.say("Lost Soul", "Oh..... Never mind then.....", {image = soulImg, font = inFont, textSpeed = "slow"})
-      Talkies.say("Cheron", "Okay bye.", {image = boatImg, font = inFont})
+      Talkies.say("Charon", "Okay bye.", {image = boatImg, font = inFont})
       return true
     end
   }
