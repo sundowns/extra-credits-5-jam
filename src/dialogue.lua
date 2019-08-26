c0dafbc128ae605078596f1ef8d4de2252a5e021
chappos <40489584+chappos@users.noreply.github.com>
2019-08-26 22:11:53 +0800

    updated dialogue

--- src/dialogue.lua
+++ src/dialogue.lua
@@ -33,8 +33,55 @@ return {
     function(boatImg, soulImg, inFont, sound)
       Talkies.say("Lost Soul", "Hello Mr. Skelington", {image = soulImg, font = inFont, talkSound = sound})
       Talkies.say("Charon", "Need a ride, bub?", {image = boatImg, font = inFont, talkSound = sound})
-      Talkies.say("Lost Soul", "Y   E   S", {image = soulImg, font = inFont, textSpeed = "slow", talkSound = sound})
+      Talkies.say("Lost Soul", "PLEASE", {image = soulImg, font = inFont, textSpeed = "slow", talkSound = sound})
       Talkies.say("Charon", "Hop in.", {image = boatImg, font = inFont, talkSound = sound})
+      Talkies.say(
+        "Lost Soul",
+        "By the way, I was looking at that turn up ahead, and uhh...",
+        {image = soulImg, font = inFont, talkSound = sound}
+      )
+      Talkies.say(
+        "Lost Soul",
+        "You might want to try holding SHIFT to tighten your turning circle for that one.",
+        {image = soulImg, font = inFont, talkSound = sound}
+      )
+      Talkies.say(
+        "Charon",
+        "Hold your SHIFT? What does that mean?",
+        {image = boatImg, font = inFont, talkSound = sound}
+      )
+      Talkies.say("Lost Soul", "Never mind...", {image = soulImg, font = inFont, textSpeed = "slow", talkSound = sound})
+      return true
+    end
+  },
+  SOUL_3 = {
+    function(boatImg, soulImg, inFont, sound)
+      Talkies.say("Charon", "Hey you called an uber?", {image = boatImg, font = inFont, talkSound = sound})
+      Talkies.say("Lost Soul", "*glares*", {image = soulImg, font = inFont, talkSound = sound})
+      Talkies.say("Charon", "Yeesh. Tough crowd.", {image = boatImg, font = inFont, talkSound = sound})
+      return true
+    end
+  },
+  SOUL_4 = {
+    function(boatImg, soulImg, inFont, sound)
+      Talkies.say(
+        "Charon",
+        "A voice in my head told me I needed to come give you safe passage.",
+        {image = boatImg, font = inFont, talkSound = sound}
+      )
+      Talkies.say(
+        "Lost Soul",
+        "Safe passage? Does such a thing exist on this cursed plane of water and whirpool? Wantonly have I wandered this waterlogged wasteland for what I wish was not a century, and still I wonder what woesome wonders await upon my departure. Well? What wisdom would you empart?",
+        {image = soulImg, font = inFont, talkSound = sound}
+      )
+      Talkies.say("Charon", ". . . . .", {image = boatImg, font = inFont})
+      return true
+    end
+  },
+  SOUL_5 = {
+    function(boatImg, soulImg, inFont, sound)
+      Talkies.say("Charon", "I'm just about done", {image = boatImg, font = inFont, talkSound = sound})
+      Talkies.say("Lost Soul", "Same t b h", {image = soulImg, font = inFont, textSpeed = "slow", talkSound = sound})
       return true
     end
   }
